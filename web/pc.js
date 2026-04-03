bc51df59480d4550d34647e8bba9f4b9f980c52c
Jesse B. Miller <jesse@jessebmiller.com>
2026-04-03 14:53:25 -0500

    wip

--- web/pc.js
+++ web/pc.js
@@ -29,11 +29,16 @@ function namespace(key) {
 }
 
 function set(key, value) {
-  window.localStorage.setItem(namespace(key), value)
+  window.localStorage.setItem(
+    namespace(key), 
+    JSON.stringify(value),
+  )
 }
 
 function get(key) {
-  return window.localStorage.getItem(namespace(key))
+  return JSON.parse(
+    window.localStorage.getItem(namespace(key))
+  )
 }
 
 function handleInputEvent(event) {
@@ -44,17 +49,92 @@ function hydrateElement(elem) {
   storedValue = get(elem.id)
   if (!storedValue) { return }
   elem.value = storedValue
+  elem.addEventListener("input", handleInputEvent)
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  persistTags = ["input", "textarea"]
-  persistElements = []
-  for (tag of persistTags) {
-    elems = document.getElementsByTagName(tag)
-    for (elem of elems) {
-      hydrateElement(elem)
-      elem.addEventListener("input", handleInputEvent)
-    }
+/* Cyberware
+ * - description
+ * - rank
+ * - quirk/malfunction[]
+ */
+function hydrateWare(ware, parentElem, index) {
+  function mkChild(div, tag, value, id, className) {
+    e = document.createElement(tag)
+    e.id = `${parentElem.id}/${id}`
+    e.classList.add(className) 
+    div.appendChild(e)
+    if (!["input", "textarea"].includes(tag)) { return e }
+    
+    e.value = value
+    return e
   }
+
+  wareDiv = document.createElement("div")
+  wareDiv.id = `${warezContainer.id}/${index}`
+  wareDiv.classList.add("ware")
+  parentElem.appendChild(wareDiv)
+
+  descriptionElem = mkChild(wareDiv, "textarea", ware.description, "description", "description")
+  rankElem = mkChild(wareDiv, "input", ware.rank, "rank", "rank")
+  quirksContainer = mkChild(wareDiv, "div", "", "quirks", "quirks")
+  
+  quirkIndex = 0
+  quirkElems = []
+  for (quirk of ware.quirks) {
+    e = mkChild(quirksContainer, "input", quirk, `${quirkIndex}/quirk`, "quirk")
+    quirkElems.push(e)
+    quirkIndex += 1
+  }
+  quirkElems.push(
+    mkChild(quirksContainer, "input", "", `${quirkIndex}/quirk`, "quirk")
+  )
+  return { descriptionElem, rankElem, quirkElems }
+}
+
+function attachInputHandlers(warez, descriptionElem, rankElem, quirkElems, i) {
+  descriptionElem.addEventListener("input", function (event) {
+    warez[i].description = event.target.value
+    set("cyberware", warez)
+  })
+  rankElem.addEventListener("input", function (event) {
+    warez[i].rank = event.target.value
+    set("cyberware", warez)
+  })
+  quirkElems.forEach(function (quirkElem, j) {
+    console.log("adding listener to", quirkElem, j)
+    quirkElem.addEventListener("input", function (event) {
+      warez[i].quirks[j] = event.target.value
+      set("cyberware", warez)
+    })
+  })
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  persistClass = "persist"
+  persistElements = []
+  elems = document.getElementsByClassName(persistClass)
+  for (elem of elems) {
+    hydrateElement(elem)
+  }
+  
+  // handle cyberware
+  testCyberware = [
+    { description: "test cyberware",
+      rank: "1",
+      quirks: ["weird", "preliminary"],
+    },
+    { description: "crawhole",
+      rank: "12",
+      quirks: ["boom", "bap"],
+    },
+  ]
+  //set("cyberware", testCyberware)
+  warezContainer = document.getElementById("cyberware")
+  warez = get("cyberware")
+  warez.forEach(function (ware, i) {
+    const { descriptionElem, rankElem, quirkElems } = hydrateWare(ware, warezContainer, i)
+    attachInputHandlers(warez, descriptionElem, rankElem, quirkElems, i)
+  })
 });
 
